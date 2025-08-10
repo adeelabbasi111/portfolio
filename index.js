bfe6a0b29da6528df7a3629d60d6c13378b38467
Adeel <adeelabbasipersonal@gmail.com>
2025-08-10 19:25:08 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const skills = ["Web Developer", "Photoshop Expert", "Video Editor",
+    "Game Developer", "App Developer", "Music Producer","Python Developer"
+];
+
+
+let skillIndex = 0;
+const skillText = document.getElementById("skillText");
+
+function updateSkill() {
+  skillText.textContent = skills[skillIndex];
+  
+  skillIndex = (skillIndex + 1) % skills.length;
+}
+
+// Change skill every 500ms
+setInterval(updateSkill, 500);
